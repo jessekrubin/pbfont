e7b28091c5c43b124899c024a21b8b6cf48c6e57
jesse <jessekrubin@gmail.com>
2026-03-12 09:58:48 -0700

    publint and bundled version (#313)
    * pub-linting
    
    * publinting
    
    * publinting
    
    * test update
    
    * donezo

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,8 @@
 import jsse from "@jsse/eslint-config";
 
 export default jsse({
-  typescript: {
-    tsconfig: ["tsconfig.json", "tsconfig.eslint.json"],
-  },
+  typescript: { tsconfig: ["tsconfig.json", "tsconfig.eslint.json"] },
   ignores: ["src/gen/glyphs_pb.ts"],
+  sortImports: true,
   off: [],
 });
--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,15 +1,32 @@
 import { defineConfig } from "tsdown";
 
-export default defineConfig({
-  entry: ["src/index.ts"],
-  splitting: false,
-  format: ["cjs", "esm"],
-  dts: true,
-  minify: false,
-  fixedExtension: false,
-  outDir: "dist",
-  sourcemap: false,
-  clean: true,
-  treeshake: true,
-  tsconfig: "./tsconfig.json",
-});
+export default defineConfig([
+  {
+    entry: ["src/index.ts"],
+    format: ["cjs", "esm"],
+    dts: true,
+    minify: false,
+    fixedExtension: false,
+    outDir: "dist",
+    sourcemap: false,
+    clean: true,
+    treeshake: true,
+    tsconfig: "./tsconfig.json",
+  },
+  {
+    entry: ["src/bundle.ts"],
+    deps: {
+      alwaysBundle: [/^@bufbuild\/protobuf/],
+      onlyBundle: [/^@bufbuild\/protobuf/],
+    },
+    format: ["cjs", "esm"],
+    dts: true,
+    minify: false,
+    fixedExtension: false,
+    outDir: "dist",
+    sourcemap: false,
+    clean: true,
+    treeshake: true,
+    tsconfig: "./tsconfig.json",
+  },
+]);
