9d92722b492dbfbb7f6b95632c6df4c781fe37ad
jessekrubin <jessekrubin@gmail.com>
2026-03-12 09:25:32 -0700

    pub-linting

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,15 +1,28 @@
 import { defineConfig } from "tsdown";
 
-export default defineConfig({
-  entry: ["src/index.ts"],
-  splitting: false,
-  format: ["cjs", "esm"],
-  dts: true,
-  minify: false,
-  fixedExtension: false,
-  outDir: "dist",
-  sourcemap: false,
-  clean: true,
-  treeshake: true,
-  tsconfig: "./tsconfig.json",
-});
+export default defineConfig([
+  {
+    entry: ["src/index.ts"],
+    format: ["cjs", "esm"],
+    dts: true,
+    minify: false,
+    fixedExtension: false,
+    outDir: "dist",
+    sourcemap: false,
+    clean: true,
+    treeshake: true,
+    tsconfig: "./tsconfig.json",
+  },
+  {
+    entry: ["src/index.ts"],
+    format: ["cjs", "esm"],
+    dts: true,
+    minify: false,
+    fixedExtension: false,
+    outDir: "dist",
+    sourcemap: false,
+    clean: true,
+    treeshake: true,
+    tsconfig: "./tsconfig.json",
+  }
+]);
